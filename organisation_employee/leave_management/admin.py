b084a4141f4dc6a4c011520a60a3b48928e724fa
Hareshwar <hareshwarmali20@gmail.com>
2025-02-14 20:13:57 +0530

    register models

--- organisation_employee/leave_management/admin.py
+++ organisation_employee/leave_management/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import EmployeeProfile, EmployeeLeave
+
 # Register your models here.
+admin.site.register(EmployeeProfile)
+admin.site.register(EmployeeLeave)
