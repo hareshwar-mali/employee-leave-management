c8ebf9098ef3a0d65f9084aa58ce6136f51e68ec
Hareshwar <hareshwarmali20@gmail.com>
2025-02-14 20:10:21 +0530

    create two modes employee and leave

--- organisation_employee/leave_management/models.py
+++ organisation_employee/leave_management/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    ACTIVE = 'Active'
+    INACTIVE = 'Inactive'
+
+    STATUS_CHOICES = [
+        (ACTIVE, 'Active'),
+        (INACTIVE, 'Inactive'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=10, unique=True)
+    password = models.CharField(max_length=4)
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES, default=ACTIVE)
+    total_cs_leaves = models.IntegerField(default=12)  # Casual Leave (CS)
+    total_e_leaves = models.IntegerField(default=12)  # Earned Leave (E)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Leave(models.Model):
+    SICK_CASUAL = 'CS'
+    EARNED = 'E'
+
+    LEAVE_TYPE_CHOICES = [
+        (SICK_CASUAL, 'Sick-Casual Leave'),
+        (EARNED, 'Earned Leave'),
+    ]
+
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    leave_type = models.CharField(max_length=2, choices=LEAVE_TYPE_CHOICES)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    status = models.CharField(max_length=15, default='Pending')  # e.g. Pending, Approved, Rejected
+
+    def __str__(self):
+        return f"{self.employee.first_name} {self.leave_type} Leave from {self.start_date} to {self.end_date}"
