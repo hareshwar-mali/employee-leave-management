441dd53b7d986dcb09210f2b5f434c085b40c248
Hareshwar <hareshwarmali20@gmail.com>
2025-02-14 20:39:17 +0530

    register model modify for display all fields

--- organisation_employee/leave_management/admin.py
+++ organisation_employee/leave_management/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from .models import EmployeeProfile, EmployeeLeave
 
-# Register your models here.
-admin.site.register(EmployeeProfile)
-admin.site.register(EmployeeLeave)
+
+class EmployeeProfileAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in EmployeeProfile._meta.fields]
+
+
+class EmployeeLeaveAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in EmployeeLeave._meta.fields]
+
+
+admin.site.register(EmployeeProfile, EmployeeProfileAdmin)
+admin.site.register(EmployeeLeave, EmployeeLeaveAdmin)
