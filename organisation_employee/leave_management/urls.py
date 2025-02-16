4fb2734b733887246decd3d445251e2836b9a590
Hareshwar <hareshwarmali20@gmail.com>
2025-02-16 17:52:40 +0530

    create new urls

--- organisation_employee/leave_management/urls.py
+++ organisation_employee/leave_management/urls.py
@@ -1,7 +1,7 @@
 from rest_framework import routers
 from django.urls import path, include, re_path
 
-from .views import admin_dashboard, modify_employee
+from .views import admin_dashboard, modify_employee, employee_login, employee_dashboard, employee_logout, apply_leave
 
 router = routers.DefaultRouter()
 
@@ -10,4 +10,8 @@ urlpatterns = [
     path("admin_dashboard/", admin_dashboard, name="admin_dashboard"),
     path("employee/add/", modify_employee, name="modify_employee"),
     path("employee/edit/<int:employee_id>/", modify_employee, name="modify_employee"),
+    path("employee_login/", employee_login, name="employee_login"),
+    path('employee_dashboard/', employee_dashboard, name='employee_dashboard'),
+    path('apply_leave/', apply_leave, name='apply_leave'),
+    path('logout/', employee_logout, name='logout'),
 ]
