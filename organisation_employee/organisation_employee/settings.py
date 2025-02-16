993c8120c20197aa6f08733a66332529749d1712
Hareshwar <hareshwarmali20@gmail.com>
2025-02-16 17:52:13 +0530

    tamplates added in dirs

--- organisation_employee/organisation_employee/settings.py
+++ organisation_employee/organisation_employee/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'organisation_employee.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
