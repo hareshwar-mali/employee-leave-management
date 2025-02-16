c0e76ba36d4aa3800ba357a7a45b3e125d7de019
Hareshwar <hareshwarmali20@gmail.com>
2025-02-16 16:08:55 +0530

    create api for admin dashboard and employee add and edit

--- organisation_employee/leave_management/urls.py
+++ organisation_employee/leave_management/urls.py
@@ -1,11 +1,13 @@
 from rest_framework import routers
 from django.urls import path, include, re_path
 
-from .views import employee_login
+from .views import admin_dashboard, modify_employee
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
     path('', include(router.urls)),
-    re_path('login/', employee_login, name='login')
+    path("admin_dashboard/", admin_dashboard, name="admin_dashboard"),
+    path("employee/add/", modify_employee, name="modify_employee"),
+    path("employee/edit/<int:employee_id>/", modify_employee, name="modify_employee"),
 ]
