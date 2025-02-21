6f6274609d042cee92d6ab410724a1819f1ff4bf
Hareshwar <hareshwarmali20@gmail.com>
2025-02-21 11:35:02 +0530

    file format

--- organisation_employee/leave_management/admin.py
+++ organisation_employee/leave_management/admin.py
@@ -5,10 +5,10 @@ from .models import EmployeeProfile, EmployeeLeave
 @admin.register(EmployeeProfile)
 class EmployeeProfileAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'user', 'first_name', 'last_name', 'phone_number', 'password', 'status', 'total_cs_leaves', 'total_e_leaves')
+        'id', 'user', 'first_name', 'last_name', 'phone_number', 'password', 'status', 'total_cs_leaves',
+        'total_e_leaves')
     search_fields = ('first_name', 'last_name', 'phone_number', 'user__username')
     list_filter = ('status',)
-    ordering = ('first_name', 'last_name')
 
 
 @admin.register(EmployeeLeave)
@@ -16,4 +16,3 @@ class EmployeeLeaveAdmin(admin.ModelAdmin):
     list_display = ('id', 'employee', 'leave_type', 'start_date', 'end_date', 'status')
     list_filter = ('leave_type', 'status')
     search_fields = ('employee__first_name', 'employee__last_name', 'employee__phone_number')
-    ordering = ('start_date',)
