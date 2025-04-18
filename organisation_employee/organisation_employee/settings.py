aec0f44f18f89d6e721fe839bb65a6ac4412d805
Hareshwar Kashinath Mali <106073932+hareshwar-mali@users.noreply.github.com>
2025-04-18 22:31:13 +0530

    Update settings.py

--- organisation_employee/organisation_employee/settings.py
+++ organisation_employee/organisation_employee/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
