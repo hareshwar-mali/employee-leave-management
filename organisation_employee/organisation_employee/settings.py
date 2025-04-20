754e8983ee5a0e2b73fc1e4621792408059b1a0f
Hareshwar Kashinath Mali <106073932+hareshwar-mali@users.noreply.github.com>
2025-04-20 23:53:45 +0530

    Update settings.py

--- organisation_employee/organisation_employee/settings.py
+++ organisation_employee/organisation_employee/settings.py
@@ -79,10 +79,15 @@ SESSION_COOKIE_AGE = 3600  # Session expires in 1 hour
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'employee_db',
+        'USER': 'employee_user',
+        'PASSWORD': 'maheshwari20',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
